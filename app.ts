2de644afd4d09783c272c90c90131b57a6d819f7
nepeckman <elijahpeckman@gmail.com>
2015-06-12 23:33:02 -0400

    web changes

--- app.ts
+++ app.ts
@@ -39,8 +39,11 @@ app.use('/chat', function(req, res, next){
 	next();
 }, chat);
 
-
-http.listen(3000, function(){
-	console.log("Listening at port 3000");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+http.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
+
 
