7b738a258402996d9e31a1c10b72808ff5e557b2
nepeckman <elijahpeckman@gmail.com>
2015-06-15 11:37:13 -0400

    web changes

--- public/Javascript/client.js
+++ public/Javascript/client.js
@@ -1,5 +1,3 @@
-$('li').click(function(){
-	console.log("hi");
-	var index = $('this').text().indexOf(":");
-	location.href = "http://localhost:3000/chat/" + $('this').text().substring(0, index);
-});
+function helloworld(string){
+	document.write(string);
+}
--- public/Javascript/room_click.js
+++
@@ -1,5 +0,0 @@
-$('li').click(function(){
-	console.log("debug log");
-	var index = $('this').text().indexOf(":");
-	location.href = "http://localhost:3000/chat/" + $('this').text().substring(0, index);
-});
